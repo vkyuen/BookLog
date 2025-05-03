0535275df782bdbd32647e24b39d509eb5fdd4bb
vkyuen <vkyuen@users.noreply.github.com>
2025-05-02 21:42:20 -0700

    Shifting to another project.
    Creating a different project with the same code, since I seem to have broken something in the original. Not sure why the Model stopped working.

---
+++ BookLog/BookLog/BookLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookLogApp.swift
+//  BookLog
+//
+//  Created by Veronica Yuen on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BookLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
