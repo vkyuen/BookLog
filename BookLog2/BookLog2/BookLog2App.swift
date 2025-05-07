5b7b4b70e6d63757482afee88ff856159f405223
vkyuen <vkyuen@users.noreply.github.com>
2025-05-06 19:39:39 -0700

    Restarting the project
    Starting a new project... Something broke in the database with the last one. Probably added to many things after having some books stored.

---
+++ BookLog2/BookLog2/BookLog2App.swift
@@ -0,0 +1,17 @@
+//
+//  BookLog2App.swift
+//  BookLog2
+//
+//  Created by Veronica Yuen on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BookLog2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookLog2/BookLog2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookLog2
+//
+//  Created by Veronica Yuen on 5/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
