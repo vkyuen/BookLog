0068a7e2775c2a1ce2a4c8d73fe3079925d0e53d
vkyuen <vkyuen@users.noreply.github.com>
2025-05-14 01:05:42 -0700

    Hiding review stuff.
    Commeting out the reveiw stuff for now. Maybe one of these days. With the notes and such, might leave it as a star review, since I don't like writing reviews.

--- BookLog3/BookLog3/Review.swift
+++ BookLog3/BookLog3/Review.swift
@@ -5,20 +5,20 @@
 //  Created by Veronica Yuen on 5/7/25.
 //
 
-import Foundation
-import SwiftData
-
-@Model
-class Review{
-    var book: ReadingMaterial
-    var dateCreated: Date
-    var rating: Int // staying with whole number stars for now
-    var note: String
-    
-    init(book: ReadingMaterial, dateCreated: Date = .now, rating: Int = 0, note: String = "") {
-        self.book = book
-        self.dateCreated = dateCreated
-        self.rating = rating
-        self.note = note
-    }
-}
+//import Foundation
+//import SwiftData
+//
+//@Model
+//class Review{
+//    var book: ReadingMaterial
+//    var dateCreated: Date
+//    var rating: Int // staying with whole number stars for now
+//    var note: String
+//    
+//    init(book: ReadingMaterial, dateCreated: Date = .now, rating: Int = 0, note: String = "") {
+//        self.book = book
+//        self.dateCreated = dateCreated
+//        self.rating = rating
+//        self.note = note
+//    }
+//}
